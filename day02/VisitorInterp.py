2ff5f7eb7b5bab09799d9414aeab1684b578c417
David Johnson Thinkpad T16 <znoop@djt16>
2023-12-02 17:41:27 -0500

    starting part2

--- day02/VisitorInterp.py
+++ day02/VisitorInterp.py
@@ -8,6 +8,20 @@ from dataclasses import dataclass
 from typing import List, Dict
 import datetime
 import time
+from math import prod
+
+
+def minLimitReduction(draws: List[Dict]) -> Dict:
+    # the argument is a list of dicts like: [{'green': 2, 'blue': 5}, {'blue': 15, 'red': 3}]
+    # return a dict containing the maximum of each individual color as another dict, e.g.,
+    # {'green': 2, 'blue': 15, 'red': 3}. this is the minimum Limit to play the game.
+    limit = {}
+    for draw in draws:
+        for color, count in draw.items():
+            limit[color] = max(count, limit.get(color, 0))
+
+    return limit
+
 
 def isWithinLimits(draw: Dict, limits: Dict) -> bool:
     # both arguments are of the form: {'green': 2, 'blue': 15, 'red': 3}
@@ -25,16 +39,26 @@ class VisitorInterp(day02Visitor):
         self.total = 0
 
     # Visit a parse tree produced by day02Parser#color_spec.
-    def visitColors(self, ctx:day02Parser.ColorsContext):
-        #print(f'{ctx.color} = {ctx.count}')
+    def visitColors(self, ctx: day02Parser.ColorsContext):
+        # print(f'{ctx.color} = {ctx.count}')
         return [ctx.color.text, int(ctx.count.text)]
 
     # Visit a parse tree produced by day02Parser#draw.
-    def visitDraw(self, ctx:day02Parser.DrawContext):
+    def visitDraw(self, ctx: day02Parser.DrawContext):
         return dict([self.visit(ctx.getChild(i)) for i in range(0, ctx.getChildCount())])
 
     # Visit a parse tree produced by day02Parser#game.
-    def visitGame(self, ctx:day02Parser.GameContext):
+    def visitGame(self, ctx: day02Parser.GameContext):
+        # part 2:
+        draws = [d for d in self.visitChildren(ctx) if d]
+        limit = minLimitReduction(draws)
+        cube_power = prod(limit.values())
+        print(f'Game {ctx.id_.text} cube_power = {cube_power}')
+
+
+
+    def visitGame_part1(self, ctx: day02Parser.GameContext):
+
         for i in range(0, ctx.getChildCount(), 1):
             draw = self.visit(ctx.getChild(i))
             if draw:
@@ -46,5 +70,3 @@ class VisitorInterp(day02Visitor):
             self.total += int(ctx.id_.text)
 
         # return self.visitChildren(ctx)
-
-
