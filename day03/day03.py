016c340cac6e6780b97dd18255ec7bfb8920f43d
David Johnson Desktop2022 <djohn89@gmail.com>
2023-12-03 10:28:44 -0500

    day03.py part2 done

--- day03/day03.py
+++ day03/day03.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from collections import defaultdict
+from math import prod
 
 def parse_schematic(input: str) -> np.array:
     # create an array representation of a text schematic input.
@@ -19,7 +20,7 @@ def neighbors8(row: int, col: int, schematic: np.array):
     for ii in range(row - 1, row + 2):
         for jj in range(col - 1, col + 2):
             if 0 <= ii < width and 0 <= jj < height:
-                yield schematic[ii, jj]
+                yield schematic[ii, jj], ii, jj
 
 
 def walk(schematic: np.array) -> int:
@@ -38,9 +39,9 @@ def walk(schematic: np.array) -> int:
                 if current_int is None:
                     current_int = int(schematic[ii, jj])
                 else:
-                    current_int = 10*current_int + int(schematic[ii, jj])
+                    current_int = 10 * current_int + int(schematic[ii, jj])
 
-                for ch in neighbors8(ii, jj, schematic):
+                for ch, _, _ in neighbors8(ii, jj, schematic):
                     if not ch.isdigit() and ch != '.':
                         found_symbol = True
             else:
@@ -52,14 +53,54 @@ def walk(schematic: np.array) -> int:
     return total
 
 
+def walk_part2(schematic: np.array) -> int:
+    # traverse a 2D graph doing a modified flood fill:
+    # when encountering any integer, combine it into the current integer
+    # also check its 8 neighbors for any '*'.
+    # keep track of which integers had a '*' neighbor, and for the '*' with exactly 2 ints, sum them up
+    total = 0
+
+    height, width = schematic.shape
+    current_int = None
+    found_symbol = None
+    gear_locations = defaultdict(list)  # keys: (ii,jj) for the gear, values: list of ints at this gear.
+    for ii in range(0, height):
+        for jj in range(0, width):
+            if schematic[ii, jj].isdigit():
+                if current_int is None:
+                    current_int = int(schematic[ii, jj])
+                else:
+                    current_int = 10 * current_int + int(schematic[ii, jj])
+
+                for ch, gi, gj in neighbors8(ii, jj, schematic):
+                    if ch == '*':
+                        found_symbol = (gi, gj)
+            else:
+                if current_int is not None and found_symbol is not None:
+                    gear_locations[found_symbol].append(current_int)
+                current_int = None
+                found_symbol = None
+
+    for loc, gears in gear_locations.items():
+        if len(gears) == 2:
+            total += prod(gears)
+
+    return total
+
+
 def main():
+    #with open("test_input.txt", "r") as f:
     with open("input.txt", "r") as f:
         input = f.read()
 
     schematic = parse_schematic(input)
     # avoid edge effect using padding
     schematic = np.pad(schematic, (1, 1), 'constant', constant_values=('.', '.'))
+    # part 1
     print(walk(schematic))
+
+    # part 2
+    print(walk_part2(schematic))
 
 
 if __name__ == '__main__':
