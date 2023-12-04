9698ee3981f7d668034be59fc21149951df01956
David Johnson Thinkpad T16 <znoop@djt16>
2023-12-04 06:26:29 -0500

    starting part2

--- day04/VisitorInterp.py
+++ day04/VisitorInterp.py
@@ -9,6 +9,7 @@ from typing import List, Dict
 import datetime
 import time
 from math import prod
+from collections import defaultdict
 
 
 class VisitorInterp(day04Visitor):
@@ -20,12 +21,36 @@ class VisitorInterp(day04Visitor):
     def visitCard(self, ctx: day04Parser.CardContext):
         have = set([int(ctx.have[i].text) for i in range(len(ctx.have))])
         win = set([int(ctx.win[i].text) for i in range(len(ctx.win))])
-        count_won = len(have.intersection(win))
-        return 2 ** (count_won-1) if count_won > 0 else 0
+        return len(have.intersection(win))
+
 
     # Visit a parse tree produced by day04Parser#start.
     def visitStart(self, ctx: day04Parser.StartContext):
+        originals = {}
+        for i in range(ctx.getChildCount()):
+            win_ = self.visit(ctx.getChild(i))
+            if win_:
+                originals[int(ctx.getChild(i).id_.text)] = self.visit(ctx.getChild(i))
+
+        copies = defaultdict(int)
+        need_to_visit = list(originals.keys())
+        while len(need_to_visit):
+            visit = need_to_visit.pop()
+            if visit in copies:
+                1
+
+
+        1
+
+    # Visit a parse tree produced by day04Parser#start.
+    def visitStart_part1(self, ctx: day04Parser.StartContext):
         for i in range(ctx.getChildCount()):
             count_won = self.visit(ctx.getChild(i))
             if count_won:
                 self.total += count_won
+
+    def visitCard_part1(self, ctx: day04Parser.CardContext):
+        have = set([int(ctx.have[i].text) for i in range(len(ctx.have))])
+        win = set([int(ctx.win[i].text) for i in range(len(ctx.win))])
+        count_won = len(have.intersection(win))
+        return 2 ** (count_won-1) if count_won > 0 else 0
