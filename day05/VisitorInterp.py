7e390a21393faca8986233f90a0eb2b8393885e0
David Johnson Thinkpad T16 <znoop@djt16>
2023-12-05 21:53:23 -0500

    day05 part1 done

--- day05/VisitorInterp.py
+++ day05/VisitorInterp.py
@@ -17,7 +17,18 @@ class VisitorInterp(day05Visitor):
     def __init__(self):
         self.seeds = set()
         self.answer = None
-        self.maps = dict()
+        self.maxmaps = 7
+        self.maps = list(range(self.maxmaps))
+        self.map2ind = {
+            'seed': 0,
+            'soil': 1,
+            'fertilizer': 2,
+            'water': 3,
+            'light': 4,
+            'temperature': 5,
+            'humidity': 6,
+            'location': 7,
+        }
 
     def visitSeed_list(self, ctx: day05Parser.Seed_listContext):
         self.seeds = set([int(ct.text) for ct in ctx.seeds])
@@ -25,13 +36,38 @@ class VisitorInterp(day05Visitor):
 
     def visitMap(self, ctx: day05Parser.MapContext):
         self.visitChildren(ctx)
-        return {
+        map_ = {
             'destination': int(ctx.destination.text),
             'source': int(ctx.source.text),
             'length': int(ctx.length.text)
         }
+        map_['s0'] = map_['source']
+        map_['s1'] = map_['source'] + map_['length']
+        map_['d0'] = map_['destination']
+        map_['d1'] = map_['destination'] + map_['length']
+        return map_
 
     def visitMaps(self, ctx: day05Parser.MapsContext):
         entries = [self.visitMap(entry) for entry in ctx.entries]
-        self.maps[ctx.source.text] = {ctx.destination.text: entries}
+        self.maps[self.map2ind[ctx.source.text]] = entries
         return self.visitChildren(ctx)
+
+    def lookup(self, map_no: int, item: int) -> int:
+        for entry in self.maps[map_no]:
+            if entry['s0'] <= item <= entry['s1']:
+                return entry['d0'] + item - entry['s0']
+        return item
+
+    def visitStart(self, ctx:day05Parser.StartContext):
+        self.visitChildren(ctx)
+        for seed in self.seeds:
+            i = seed
+            for j in range(self.maxmaps):
+                i = self.lookup(j, i)
+
+            if self.answer is None:
+                self.answer = i
+            else:
+                self.answer = min(self.answer, i)
+
+        return self.answer
