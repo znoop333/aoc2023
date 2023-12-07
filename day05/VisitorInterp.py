9bf55bbebb21b29e0a42580a50d56e78ae372d52
David Johnson Desktop2022 <djohn89@gmail.com>
2023-12-07 05:44:03 -0500

    trying again on d5.p2: inclusive range: [start, stop] with fixed step size 1

--- day05/VisitorInterp.py
+++ day05/VisitorInterp.py
@@ -1,3 +1,4 @@
+import dataclasses
 import sys
 from antlr4 import *
 from gen.day05Lexer import day05Lexer
@@ -11,6 +12,111 @@ import time
 from math import prod
 from collections import defaultdict
 import functools
+
+
+@dataclasses.dataclass
+class iRange:
+    # inclusive range: [start, stop] with fixed step size 1
+    # normally start<=stop.
+    start: int
+    stop: int
+
+    def isEmpty(self):
+        return self.start > self.stop
+
+    def __len__(self):
+        return 0 if self.isEmpty() else self.stop - self.start + 1
+
+    @staticmethod
+    def emptyIRange():
+        return iRange(0, -1)
+
+    def __add__(self, shift_):
+        if isinstance(shift_, int):
+            return iRange(self.start + shift_, self.stop + shift_)
+
+
+def IntersectionWithTransformation(a: iRange, b: iRange, tx_shift: int) -> List[iRange]:
+    # always returns 3 iRanges, but some of them can be None
+
+    # case 0: empty ranges
+    if a.isEmpty() or b.isEmpty():
+        return [None, None, None]
+
+    # case 1: identical ranges
+    if a == b:
+        return [None, a + tx_shift, None]
+
+    overlap = iRange(max(a.start, b.start), min(a.stop, b.stop))
+    if overlap.isEmpty():
+        # case 2: non-overlapping ranges
+        return [None, a, None]
+    # past this point, there must be some non-trivial overlap between a and b
+    overlap_shifted = iRange(overlap.start, overlap.stop) + tx_shift
+
+    # case 3: a is entirely contained in b
+    if b.start <= a.start and a.stop <= b.stop:
+        return [None, overlap_shifted, None]
+
+    # case 6: b is entirely contained in a. a is split into 3 pieces: before, overlap, and after
+    if a.start < b.start and b.stop < a.stop:
+        before = iRange(a.start, b.stop - 1)
+        after = iRange(b.stop + 1, a.stop)
+        return [before, overlap_shifted, after]
+
+    # case 5: a begins before b and also overlaps it. part of a is not transformed.
+    if a.start < b.start and a.stop <= b.stop:
+        before = iRange(a.start, b.start - 1)
+        return [before, overlap_shifted, None]
+
+    # case 4: a ends after b and also overlaps it. part of a is not transformed.
+    if a.start >= b.start and a.stop > b.stop:
+        after = iRange(b.stop + 1, a.stop)
+        return [None, overlap_shifted, after]
+
+    return [None, overlap_shifted, None]
+
+
+def IntersectionWithTransformationTests():
+    a = iRange(50, 3)
+    assert a.isEmpty()
+
+    a = iRange(51, 60)
+    assert len(a) == 10
+    assert a + (-50) == iRange(1, 10)
+
+    b = iRange(55, 65)
+    before, overlap_shifted, after = IntersectionWithTransformation(a, b, -50)
+    assert len(overlap_shifted) == 6
+    assert before == iRange(51, 54)
+    assert overlap_shifted == iRange(5, 10)
+    assert after is None
+
+    c = iRange(1000, 3000)
+    before, overlap_shifted, after = IntersectionWithTransformation(a, c, 0)
+    assert before is None
+    assert overlap_shifted == a
+    assert after is None
+
+    before, overlap_shifted, after = IntersectionWithTransformation(b, a, 0)
+    assert before is None
+    assert overlap_shifted == iRange(55, 60)
+    assert after == iRange(61, 65)
+
+    i1 = iRange(10, 10)
+    i2 = iRange(20, 20)
+    assert len(i1) == 1
+    assert i1 + 10 == i2
+
+    before, overlap_shifted, after = IntersectionWithTransformation(i1, i2, 10)
+    assert before is None
+    assert overlap_shifted == i1
+    assert after is None
+
+    before, overlap_shifted, after = IntersectionWithTransformation(i1, i1, 0)
+    assert before is None
+    assert overlap_shifted == i1
+    assert after is None
 
 
 class VisitorInterp(day05Visitor):
@@ -29,6 +135,7 @@ class VisitorInterp(day05Visitor):
             'humidity': 6,
             'location': 7,
         }
+        IntersectionWithTransformationTests()
 
     def visitSeed_list(self, ctx: day05Parser.Seed_listContext):
         # self.seeds = set([int(ct.text) for ct in ctx.seeds])
@@ -36,7 +143,7 @@ class VisitorInterp(day05Visitor):
         if len(ctx.seeds):
             for i in range(0, len(ctx.seeds), 2):
                 self.seeds.append(
-                    range(int(ctx.seeds[i].text), int(ctx.seeds[i].text) + int(ctx.seeds[i + 1].text) + 1))
+                    iRange(int(ctx.seeds[i].text), int(ctx.seeds[i].text) + int(ctx.seeds[i + 1].text)))
         return self.visitChildren(ctx)
 
     def visitMap(self, ctx: day05Parser.MapContext):
@@ -69,15 +176,25 @@ class VisitorInterp(day05Visitor):
             i = self.lookup(j, i)
         return i
 
+    def search_ranges(self, a: iRange) -> iRange:
+        active_ranges = [a]
+        for j in range(self.maxmaps):
+            if not len(active_ranges) > 0:
+                return []
+
+            i = self.lookup(j, i)
+        return i
+
     def visitStart(self, ctx: day05Parser.StartContext):
         self.visitChildren(ctx)
         for rng in self.seeds:
-            for seed in rng:
-                i = self.search_maps(seed)
+            soln = self.search_ranges(rng)
+            # for seed in rng:
+            #     i = self.search_maps(seed)
 
-                if self.answer is None:
-                    self.answer = i
-                else:
-                    self.answer = min(self.answer, i)
+            if self.answer is None:
+                self.answer = i
+            else:
+                self.answer = min(self.answer, i)
 
         return self.answer
