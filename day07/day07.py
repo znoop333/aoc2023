7b76b331ce69cd20c6b0847f50eded13fd243b76
David Johnson Thinkpad T16 <znoop@djt16>
2023-12-07 18:03:33 -0500

    outline for day07.py

---
+++ day07/day07.py
@@ -0,0 +1,19 @@
+import numpy as np
+from collections import defaultdict
+from math import prod, sqrt, floor, ceil
+
+
+def parse_input(input: str):
+    return
+
+
+def main():
+    # with open("input.txt", "r") as f:
+    with open("test_input.txt", "r") as f:
+        input = f.read()
+
+    parse_input(input)
+
+
+if __name__ == '__main__':
+    main()
