1b9e3ee534395fd007cea8927806b22cabd730e8
David Johnson Thinkpad T16 <znoop@djt16>
2023-12-11 06:30:02 -0500

    adjacency checks

--- day10/day10.py
+++ day10/day10.py
@@ -24,13 +24,24 @@ def neighbors4(row: int, col: int, graph: np.array):
                '-': [(0, 1), (0, -1)],
                '|': [(-1, 0), (1, 0)],
                'S': [(-1, 0), (1, 0), (0, 1), (0, -1)],
+               '.': [],
                }
 
-    # 'S' is very special!
+    # 'S' is very special! it can connect to two pipes in any direction, but only if the target direction is a valid
+    # connector. e.g., if 'S' is acting like 'F', it can connect on the right to '7', '-', 'J', but not 'L', '|', or '.'
+    # I'll check this requirement for all nodes, even though only 'S' really requires it if the graph is valid.
+    # an invalid graph might have something like 'LLL' which should be illegal because adjacent 'L' cannot connect.
+
+    valid_targets = {
+        (0, 1): ['7', '-', 'J'],
+        (0, -1): ['L', '-', 'F'],
+        (1, 0): ['L', '|', 'J'],
+        (-1, 0): ['7', '|', 'F'],
+    }
 
     for dr, dc in offsets[node]:
         ii, jj = row + dr, col + dc
-        if 0 <= ii < width and 0 <= jj < height:
+        if 0 <= ii < width and 0 <= jj < height and graph[ii, jj] in valid_targets[(dr, dc)]:
             yield graph[ii, jj], ii, jj
 
 
